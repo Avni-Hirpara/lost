7aa0a89988f684a0f1a3c447d450224578f492b7
avnih <avnih0@gmail.com>
2020-11-17 14:00:43 +0530

    Modified import_examples.sh to import all pipe project within pipes directory and import all label tree within label_trees directory.

--- backend/lost/cli/import_examples.sh
+++ backend/lost/cli/import_examples.sh
@@ -1,10 +1,19 @@
 if [ ${ADD_EXAMPLES} = "True" ]; then
-python3 import_pipe_project.py '../pyapi/examples/pipes/ocr_doc_anno'
-python3 import_label_tree.py '../pyapi/examples/label_trees/pancard_label_tree.csv'
-    if [ ${ADD_AI_EXAMPLES} = "True" ]; then
-    python3 import_pipe_project.py '../pyapi/examples/pipes/sia'
-    python3 import_pipe_project.py '../pyapi/examples/pipes/mia'
-    python3 import_pipe_project.py '../pyapi/examples/pipes/two_stage'
-    fi
-cp -r ../pyapi/examples/images/* $LOST_HOME/data/media
+pipe_dir='../pyapi/examples/pipes'
+pipe_childs=$(find $pipe_dir ! -path $pipe_dir -type d)
+for each in $pipe_childs
+do
+    python3 import_pipe_project.py $each
+done
+
+
+label_csv=$(find ../pyapi/examples/label_trees/ -type f -name "*.csv")
+for each in $label_csv
+do
+    python3 import_label_tree.py $each
+done
+# python3 import_pipe_project.py '../pyapi/examples/pipes/ocr_doc_anno'
+# python3 import_label_tree.py '../pyapi/examples/label_trees/pancard_label_tree.csv'
+
+# cp -r ../pyapi/examples/images/* $LOST_HOME/data/media
 fi
